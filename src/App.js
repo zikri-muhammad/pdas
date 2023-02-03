9961b0b6cb1346a2ea72ebd767a313469b9bdf84
zikri.wetalk <zikri.wetalk@gmail.com>
2023-02-03 11:26:50 +0700

    building with reusable components and here i lern how to implement props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bulma/css/bulma.css';
+import ProfileCard from "./ProfileCard"
+import AlexaImage from "./images/alexa.png"
+import CortanaImage from "./images/cortana.png"
+import SiriImage from "./images/siri.png"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <div className='hero is-primary'>
+                <div className='hero-body'>
+                    <div className='title'>Personal Digital Assistants</div>
+                </div>
+            </div>
+
+            <div className='container'>
+                <div className='section'>
+                    <div className='columns'>
+                        <div className='column'>
+                            <ProfileCard 
+                                title="alexa" 
+                                handle="@alexa99" 
+                                image={AlexaImage}  
+                                description="Alexa create mode"
+                            />
+                        </div>
+                        <div className='column'>
+                            <ProfileCard 
+                                title="Cortana" 
+                                handle="@cortana19" 
+                                image={CortanaImage} 
+                                description="Cortana create mode"
+                            />
+                        </div>
+                        <div className='column'>
+                            <ProfileCard 
+                                title="Siri" 
+                                handle="@siri01" 
+                                image={SiriImage} 
+                                description="Siri was make by apple"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/ProfileCard.js
@@ -0,0 +1,21 @@
+function ProfileCard({title, handle, image, description}) {
+    // const {title, handle} = props
+    return (
+        <div className="card">
+           <div className="card-image">
+                <figure className="image is-1by1">
+                    <img src={image} alt="pda logo" />
+                </figure>
+           </div>
+           <div className="card-content">
+                <div className="media-content">
+                    <p className="title is-4">Title is {title} </p>
+                    <p className="subtitle is 6">twiter {handle} </p>
+                    <p className="subtitle is 6">description {description} </p>
+                </div>
+           </div>
+        </div>
+    )
+}
+
+export default ProfileCard
